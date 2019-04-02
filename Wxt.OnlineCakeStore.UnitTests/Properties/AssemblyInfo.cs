f88f42596ec28459d95949551e226d61b367cf8e
xintian.wang23@gmail.com <xintian.wang23@gmail.com>
2019-04-02 17:36:28 -0400

    Create Wxt.OnlineCakeStore solution 1 [ADD] Create sln file 2 [ADD] Add WebApp, Domain, UnitTests projects 3 [MOD] Add references between projects

---
+++ Wxt.OnlineCakeStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Wxt.OnlineCakeStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Wxt.OnlineCakeStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d389b36-7351-44bc-b23d-a9fdf2b97ab8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
